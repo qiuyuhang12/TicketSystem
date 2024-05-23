e18f8f318499ca472f03aa704ac99e7522642690
grace <2094863012@qq.com>
2024-05-23 23:32:37 +0800

    扩大

--- TicketSystem.hpp
+++ TicketSystem.hpp
@@ -2012,10 +2012,10 @@ private://主分支函数
 public:
     TicketSystem() : Username_ToUser("Username_ToUser",10000*2), Username_ToOrders("Username_ToOrders",10000*2),
                      TrainID_ToTrain("TrainID_ToTrain",10000*2),
-                     TrainIDDate_ToReleasedTrain("TrainIDDate_ToReleasedTrain",100*2,20*2),
+                     TrainIDDate_ToReleasedTrain("TrainIDDate_ToReleasedTrain",100*2,200),
 //                     Station_TrainID_ToTrainForQT("Station_TrainID_ToTrainForQT"),
-                     Station_TrainID_ToTrainForQTOlyId("Station_TrainID_ToTrainForQTOlyID",100*2,20*2),
-                     TrainIDDate_ToPends("TrainIDDate_ToPends",100*2,20*2) {
+                     Station_TrainID_ToTrainForQTOlyId("Station_TrainID_ToTrainForQTOlyID",100*2,200),
+                     TrainIDDate_ToPends("TrainIDDate_ToPends",100*2,200) {
 //        std::cout << "User_Size:  " << sizeof(User) << std::endl <<
 //                  "Train_Size:  " << sizeof(Train) << std::endl <<
 //                  "releasedTrain_Size:  " << sizeof(releasedTrain) << std::endl <<
